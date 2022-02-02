7d3115dfa9d79d88428c9ad01f3a9b291c8d067f
agolautner <lautner.agoston@gmail.com>
2022-02-02 15:19:20 +0100

    feb 2 second half

--- cra-blog-app/src/App.jsx
+++ cra-blog-app/src/App.jsx
@@ -1,6 +1,25 @@
 import { useState } from 'react';
 import './App.css';
 
+function Post({item, remove, edit}) {
+
+
+  const [newTitle, setNewTitle] = useState('');
+  const [newText, setNewText] = useState('');
+
+  return (
+    <div key={item.id}>
+      <h2>{item.title}</h2>
+      <p>{item.postText}</p>
+      <p>{item.date.toLocaleString()}</p>
+      <input onChange={(e) => setNewTitle(e.target.value)} value={newTitle} placeholder='New post title' />
+      <input onChange={(e) => setNewText(e.target.value)} value={newText} placeholder='New post text' />
+      <button onClick={() => edit(item.id, newTitle, newText)} >Edit</button>
+      <button onClick={() => remove(item.id)}>Delete</button>
+    </div>
+  )
+}
+
 function App() {
 
   const [posts, setPosts] = useState([]);
@@ -20,6 +39,23 @@ function App() {
     setPostText('');
   } 
 
+  function remove(id) {
+    setPosts(posts.filter((post) => post.id !== id));
+  }
+
+  function edit(id, newTitle, newText) {
+    console.log(`editing post nr. ${id}`);
+    const newList = [];
+    for (let post of posts) {
+      if (post.id === id) {
+        post.title = newTitle;
+        post.postText = newText;
+      }
+      newList.push(post);
+    }
+    setPosts(newList);
+  }
+
   return (
     <div className="App">
       <h1>Blog Posts</h1>
@@ -33,15 +69,7 @@ function App() {
 
       {posts.map((item) => {
         return (
-          <div key={item.id}>
-            <h2>{item.title}</h2>
-            <p>{item.postText}</p>
-            <p>{item.date.toLocaleString()}</p>
-            <input placeholder='New post title' />
-            <input placeholder='New post text' />
-            <button>Edit</button>
-            <button>Delete</button>
-          </div>
+          <Post item={item} key={item.id} remove={remove} edit={edit}/>
         )
       })}
       
